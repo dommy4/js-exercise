faabace701092065f1118dc67e37b446276318f7
Wwashy <washingndi@gmail.com>
2019-10-25 16:35:20 +0300

    actual copy of array and initial length of array

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -211,7 +211,7 @@ describe("basics", function() {
             // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
             // ✏️ ADD CODE HERE ✏️
 
-            ////////////////////////////////////////////////////////////////////
+            const firstArray =Array.from(firstArray);///////////////////////////////////////////////////////////////////
 
             expect(firstArray).to.eql([1, 2, 3]);
             expect(secondArray).to.eql([1, 2, 3]);
@@ -231,7 +231,8 @@ describe("basics", function() {
             ////////////////////////////////////////////////////////////////////
             // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
             // ✏️ ADD CODE HERE ✏️
-
+            const longArray=[]
+            longArray.length =100;
             ////////////////////////////////////////////////////////////////////
 
             expect(longArray).to.be.instanceOf(Array);
