a5d7f30083b51b7c847fd172e7e60e602f3ca49c
Wwashy <washingndi@gmail.com>
2019-10-25 17:49:12 +0300

    bracket notation

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -339,7 +339,7 @@ describe("basics", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            propertyName = "secondProperty";
             ////////////////////////////////////////////////////////////////////
 
             // That's the same property access as above, but now we get back
