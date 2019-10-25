c26b7c4585370da68b52ddf8c0e0df200886cae7
Wwashy <washingndi@gmail.com>
2019-10-25 16:32:05 +0300

    undefined property and deletion of property

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -152,7 +152,7 @@ describe("basics", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            object.someProperty =undefined;
             ////////////////////////////////////////////////////////////////////
 
             // object.someProperty is undefined...
@@ -171,7 +171,7 @@ describe("basics", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            delete object.someProperty;
             ////////////////////////////////////////////////////////////////////
 
             // object.someProperty is undefined...
